bc6012e9500556adb08d8d7deb39b2b14f12250e
ASPNetPioneers <tareqmjoudeh@gmail.com>
2022-12-20 19:50:39 +0300

    Completed Introduction to Methods: instance, static, value, ref, out, and array params keyword.

---
+++ CSharpDaySix/Homework/4_CheckIfOddNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace _4_CheckIfOddNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("4. Check if an array contains an odd number.\n");
+
+            bool isOdd = false;
+            int[] numbers = { 1, 2, 3, 5, 6, 8, 20 };
+
+            for (int i = 0; i < numbers.Length; i++) 
+                {
+                 if (numbers[i] % 2 != 0)
+                    {      
+                        isOdd = true;
+                    break;
+                    }
+                 else
+                    {
+                        isOdd= false;                        
+                    }
+                }
+            Console.WriteLine($"Check if any array contains an odd number? {isOdd}");
+        }        
+    }
+}
---
+++ CSharpDaySix/Methods/Methods/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Methods\n");
+
+            // calling instance methods
+            // In order to call instance methods, we need first to create an object of the class the method is in.
+            Program program = new Program();
+            //  program.EvenNumber(40);
+
+            // calling static methods
+            /* 
+             Called directly from it's class, and not an object.
+             */
+            Console.WriteLine("");
+            int addResult = Add(10, 20);
+            Console.WriteLine(addResult);
+
+            Console.WriteLine("");
+            int i = 0;
+            ValueTypeMethod(i);
+            i = 100;
+            Console.WriteLine(i           );
+
+
+            Console.WriteLine("");
+            int x = 0;
+            RefTypeMethod(ref i);
+            x = 100;
+            Console.WriteLine(x);
+
+        }
+         // Methods
+         public void EvenNumber(int target)
+        {
+            int start = 0;
+            while (start <= target)
+            {
+                Console.WriteLine(start);
+                start +=2;
+            }
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static void ValueTypeMethod(int j) // value parameter
+        {
+            Console.WriteLine(j);
+        }
+        public static void RefTypeMethod(ref int j) // value parameter
+        {
+            Console.WriteLine(j);
+        }
+
+        // out 
+      
+        // array params keyword
+    }
+}
