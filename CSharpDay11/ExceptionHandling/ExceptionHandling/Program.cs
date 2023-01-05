6c838cd35616aa9cdb120d2fe0a21bc32cfb92f5
TareqJudehGithub <tareq.joudeh@gmail.com>
2023-01-05 21:04:55 +0200

    Completed Excepion Handling. Completed Enums.

---
+++ CSharpDay11/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Exception Handling\n");
+
+            try
+            {
+                StreamReader streamReader = new StreamReader(@"E:\AAAMyText.txt");  // Define reader obj from SteamReader class
+                var readFile = streamReader.ReadToEnd();                                          // Read file                
+
+                Console.WriteLine(readFile);
+                streamReader.Close();                                                                       // Close file
+            }
+            catch (FileNotFoundException err)
+            {
+                Console.WriteLine("File not found.");
+            }
+            catch (Exception err)  // base class exception. Should not be on top in case if other specific exceptions were implemented.
+            {
+                Console.WriteLine($"Error: {err.Message}\n");                       // production
+                Console.WriteLine($"Error Stack Trace: {err.StackTrace}");     // development
+            }
+
+            finally
+            {
+                Console.WriteLine("\nFinished compiling");
+            }
+        }
+    }
+}
